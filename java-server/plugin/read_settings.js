c1e010f3c95eace1b29a81efb2d484360b4a116c
Luke C-lpahler Pahler <41704787+adieuleveut@users.noreply.github.com>
2025-06-23 10:52:05 -0600

    Local IP to Domain

--- java-server/plugin/read_settings.js
+++ java-server/plugin/read_settings.js
@@ -1,6 +1,6 @@
 export function readSettings() {
     return chrome.storage.sync.get({
-        "server_name": '192.168.1.105:8080',
+        "server_name": 'operationlinux.com:8080',
         "passcode": 'none'
       });
 }
